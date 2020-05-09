b799071553ea653dce5e9187d2cde1dac9f11188
Danny <danny.r.huang@gmail.com>
2020-05-09 15:28:13 -0700

    add back frontend

---
+++ frontend/build/precache-manifest.48ce13e5c017567bc8d01f3cb7ef7bd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2512436bf1280074ac0dc3c4eb04a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c019b078aef5667c97f",
+    "url": "/static/js/2.fa2137df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fa2137df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6197c038434c85285393",
+    "url": "/static/js/main.d3ad4dcc.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
